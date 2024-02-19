9bf82baa14d88303572f4d31f20759c861810f65
ashraf56 <ashrafulfahim07@gmail.com>
2024-02-19 19:46:55 +0600

    Header updated

--- src/app/Compoent/Header/Header.jsx
+++ src/app/Compoent/Header/Header.jsx
@@ -1,14 +1,18 @@
 import React from 'react';
+import { IoSearchCircleSharp } from "react-icons/io5";
 
 const Header = () => {
     return ( 
-        <div className='flex gap-6 mx-auto justify-center items-center bg-base-200  md:min-h-[90v] p-10'>
-           <div className='lg:w-3/6'>
-           <h1 className='font-extrabold text-xl uppercase'>Learn Easily, Master Seamlessly </h1>
-            <p>The ultimate resource for learning programming in Bengali. Find incredible training and the latest information on our website. Step-by-step examples and clear explanations provide an unparalleled opportunity to learn programming in a personalized and professional manner</p>
+        <div className='flex-col lg:flex gap-2 mx-auto justify-center items-center bg-base-200  md:min-h-[90v] p-20'>
+           <div className='w-full lg:w-3/6 text-center'>
+           <h1 className='font-extrabold text-4xl uppercase'>Learn Easily, Master Seamlessly </h1>
+            <p className='text-xs'>The ultimate resource for learning programming in Bengali. Find incredible training and the latest information on our website. Step-by-step examples and clear explanations provide an unparalleled opportunity to learn programming in a personalized and professional manner</p>
            </div>
-           <div className='lg:3/6'>
-            <input type="text" className='input ' /> 
+           <div className=' w-full lg:w-3/6 mx-auto mt-5 '>
+            <div className='bg-base-100 mx-auto  flex rounded-lg input items-center max-w-md'>
+            <span className='  text-xl '><IoSearchCircleSharp className='text-4xl' /></span><input type="text" className='bg-base-100  px-3 outline-none ' placeholder='Search Here' /> 
+            </div>
+
            </div>
         </div>
     );
