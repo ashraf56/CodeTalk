81ef04e78c1314fc5490f8e2d9c5becdc63112bf
ashraf56 <ashrafulfahim07@gmail.com>
2024-02-27 15:35:03 +0600

    restart

--- src/app/Compoent/AllBlogs/Allblogs.jsx
+++ src/app/Compoent/AllBlogs/Allblogs.jsx
@@ -3,23 +3,47 @@ import Link from 'next/link';
 import React from 'react';
 
 const Allblogs = async () => {
-    // let data = await getBlogs()
+     let data = await getBlogs() 
 
     return (
-        <div >
-            <h1>Read letest Blogs</h1>
-            <div className='grid grid-cols-3'>
+        <div className='container mx-auto' >
+            <h1 className='text-white py-10 font-bold'>Read letest Blogs</h1>
+            <div className='grid grid-cols-3  gap-3 mx-auto  p-10'>
 
 
-                <div className='card w-96 shadow-2xl p-2 bg-black'>
+            <div className='card w-96 shadow-2xl p-2 bg-black'  >
                     <div className="card-body">
-                        <h2 >
-                            Shoes!
-                            <div className="badge badge-secondary">NEW</div>
-                        </h2>
+                        <div >
+                            <span className='pe-3'>Mar 1, 24</span>
+                            <div className="badge badge-secondary mx-2">React </div>
+                        </div>
                         <p className='text-2xl font-bold text-white'>If a dog chews shoes whose shoes does he choose?</p>
                         <div className="card-actions justify-start my-3">
-                           <p className='text-white font-semibold '>  Read more </p>
+                           <div className='text-white font-semibold '>  Read more </div>
+                        </div>
+                    </div>
+                </div>
+                <div className='card w-96 shadow-2xl p-2 bg-black'  >
+                    <div className="card-body">
+                        <div >
+                            <span className='pe-3'>Mar 1, 24</span>
+                            <div className="badge badge-secondary mx-2">React </div>
+                        </div>
+                        <p className='text-2xl font-bold text-white'>If a dog chews shoes whose shoes does he choose?</p>
+                        <div className="card-actions justify-start my-3">
+                           <div className='text-white font-semibold '>  <Link href={`/detailblog`}>Read more</Link> </div>
+                        </div>
+                    </div>
+                </div>
+                <div className='card w-96 shadow-2xl p-2 bg-black'  >
+                    <div className="card-body">
+                        <div >
+                            <span className='pe-3'>Mar 1, 24</span>
+                            <div className="badge badge-secondary mx-2">React </div>
+                        </div>
+                        <p className='text-2xl font-bold text-white'>If a dog chews shoes whose shoes does he choose?</p>
+                        <div className="card-actions justify-start my-3">
+                           <div className='text-white font-semibold '>  <Link href={`/detailblog`}>Read more</Link> </div>
                         </div>
                     </div>
                 </div>
