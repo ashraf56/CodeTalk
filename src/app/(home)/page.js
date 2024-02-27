c3bf564f388596b13d53ac1477ade8077446a3d0
ashraf56 <ashrafulfahim07@gmail.com>
2024-02-27 19:49:02 +0600

    Home layout created

---
+++ src/app/(home)/page.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Header from '../Compoent/Header/Header';
+import Allblogs from '../Compoent/AllBlogs/Allblogs';
+
+const Homecompo = () => {
+    return (
+        <div>
+            
+      <Header></Header>
+      <Allblogs></Allblogs>
+        </div>
+    );
+};
+
+export default Homecompo;
