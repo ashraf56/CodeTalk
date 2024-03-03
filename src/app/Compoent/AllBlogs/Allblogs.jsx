d1650d1bf4dac1d072c3097d8d5838804e080699
ashraf56 <ashrafulfahim07@gmail.com>
2024-03-03 11:48:34 +0600

    try to fix error

--- src/app/Compoent/AllBlogs/Allblogs.jsx
+++ src/app/Compoent/AllBlogs/Allblogs.jsx
@@ -15,7 +15,7 @@ const Allblogs = async () => {
         <div key={d?._id} className='card w-96 h-full shadow-2xl border-2 border-white hover:border-blue-600 p-2 bg-black transition duration-300 ease-linear transform text-white hover:transition-3s hover:bg-white hover:text-black hover:bg-gradient-to-tl '  >
                     <div className="card-body">
                         <div >
-                            <span className='pe-3'>{d.date}</span>
+                            <span className='pe-3'>{d?.date}</span>
                             <div className="badge badge-secondary mx-2">React </div>
                         </div>
                         <p className='text-2xl font-bold  '>{d?.title}</p>
